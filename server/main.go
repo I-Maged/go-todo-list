abf9cfec3877e80693c481268efb1ef5176459d0
I-Maged <magedhanafy@gmail.com>
2023-06-17 10:53:11 +0300

    connect to MongoDB

--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/I-Maged/go-todo-list/server/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello from todo list")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+	router.GET("/", routes.GetTodos)
+
+	router.Run(":" + port)
 }
---
+++ server/routes/entries.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var entryCollection *mongo.Collection = OpenCollection(Client, "TodoList")
+
+func GetTodos(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+}
