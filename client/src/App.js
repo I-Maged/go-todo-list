d9efc8786a3abad4a12ec8f052e4f5cd630ab879
I-Maged <magedhanafy@gmail.com>
2023-06-19 07:43:48 +0300

    initial client commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center my-3">
+      <h1 className="text-5xl font-bold">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
